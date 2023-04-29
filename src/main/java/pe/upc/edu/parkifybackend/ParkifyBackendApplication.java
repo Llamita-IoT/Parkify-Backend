21084f3d19547e84369d8cedec4f8d60b1746656
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2023-04-29 12:33:18 -0500

    Initial release

---
+++ src/main/java/pe/upc/edu/parkifybackend/ParkifyBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.parkifybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/upc/edu/parkifybackend/ParkifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.parkifybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
